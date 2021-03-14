7de5e2409b3f7b821175a4cb083c1f8d45c05560
Vishal <Vishal@mx>
2021-03-15 02:21:49 +0530

    Added sunstraction in main.c

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 
 int main()
 {
-	int a,b,c;
+	int a,b,c,d;
 	printf("Enter a b: ");
 	scanf("%d %d",&a,&b);
 	c = sum(a,b);
+	d = a - b;
 	printf("c is : %d\n",c);
+	printf("d is : %d\n",d);
 	return 0;
 }
 	
